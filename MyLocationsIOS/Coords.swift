02eb90ae071abe7db182f93663bcc442a1fba666
PhilippEdrichCodes <philipp@edrich.codes>
2022-11-17 17:32:15 +0100

    design DetailScene

---
+++ MyLocationsIOS/Coords.swift
@@ -0,0 +1,11 @@
+//
+//  Coords.swift
+//  MyLocationsIOS
+//
+//  Created by Philipp Edrich on 17.11.22.
+//
+
+struct Coords {
+    var longitude: Float
+    var latitude: Float
+}
---
+++ MyLocationsIOS/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  MyLocationsIOS
+//
+//  Created by Philipp Edrich on 17.11.22.
+//
+
+struct Location {
+    var name: String
+    var description: String
+    var w3w: String
+    var coords: Coords
+}
